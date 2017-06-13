3ae839285c0fe360e2c9a0b5c467d3a3bc6e43b5
wlanowski <nitzschejohn@gmail.com>
2017-06-13 17:27:30 +0200

    Update footerlink zu Github

--- wip/inc/footer.content.php
+++ wip/inc/footer.content.php
@@ -3,7 +3,7 @@
  <?php require_once('globalconfig.php'); ?>
         <footer>
           <div class="pull-right">
-            <?php echo $projectxname ?> - Ein Projekt von John Nitzsche. Informationen und Quelltext: <a href="https://github.com/wlanowski/tktestmap">github.com</a>
+            <?php echo $projectxname ?> - Ein Projekt von John Nitzsche. Informationen und Quelltext: <a href="https://github.com/wlanowski/geobahn">github.com</a>
           </div>
           <div class="clearfix"></div>
         </footer>
